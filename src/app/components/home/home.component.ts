6e350c6ea59c136046c4abbea1adc1673ac5a38c
Roushan Raj <roushan.raj@kiwitech.com>
2024-05-14 11:12:23 +0530

    text

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  title="Home"
+  constructor(private titleService:Title,private meta:Meta){}
+  ngOnInit(){
+    this.titleService.setTitle(this.title)
+    this.meta.updateTag({name:"description",content:"This is Practice App"})
+  }
 }
