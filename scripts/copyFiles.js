5f0425e9c27fab52a230d5089b6b5e2c3e843ed3
Roushan Raj <roushan.raj@kiwitech.com>
2024-05-14 12:00:17 +0530

    text

--- scripts/copyFiles.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs-extra")
-
-(async ()=>{
-    const src = "./dist"
-    const copy = "./netlify/functions/dist"
-
-    await fs.remove(copy)
-    await fs.copy(src, copy)
-})();
